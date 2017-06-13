66e14f5226947018157853e6346a0bb1b6d5c3ff
Zac Collier <zacacollier@gmail.com>
2017-06-13 17:22:51 -0500

    Add store, initialize devtools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+const initialState = {
+  API: {
+    status: 'idle'
+  },
+}
 
+const rootReducer = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+    return state
+  }
+};
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(ReduxThunk))
+);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
