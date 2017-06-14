5c9036e01da065b5835074809113dd609daef2ee
Zac Collier <zacacollier@gmail.com>
2017-06-14 14:55:55 -0500

    Update Topic, tweak styles

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import unknown from '../assets/images/unknown.png';
 import './Topic.css';
 
@@ -9,12 +10,27 @@ const Topic = ({ topic }) => (
       src={topic.data.thumbnail}
       alt={unknown}
     />
-    <p className='Topic-author'>{topic.data.author}</p>
-    <p className='Topic-title'>{topic.data.title}</p>
-    <p className='Topic-num_comments'>{topic.data.num_comments}</p>
-    <p className='Topic-ups'>{topic.data.ups}</p>
-    <p className='Topic-downs'>{topic.data.downs}</p>
+    {
+      /* NB:
+        `Topic-details` is `flex-direction: column`,
+         `Topic-stats' is `flex-direction: row`.
+         Hence the need for extra `<div>`s.
+      */
+    }
+    <div className='Topic-details'>
+      <p className='Topic-author'>{topic.data.author}</p>
+      <p className='Topic-title'>{topic.data.title}</p>
+      <div className='Topic-stats'>
+        <p className='Topic-num_comments'>{topic.data.num_comments} comments</p>
+        <p className='Topic-ups'>{topic.data.ups} ups</p>
+        <p className='Topic-downs'>{topic.data.downs} downs</p>
+      </div>
+    </div>
   </div>
 );
+
+Topic.propTypes = {
+  topic: PropTypes.object.isRequired,
+}
 
 export default Topic;
